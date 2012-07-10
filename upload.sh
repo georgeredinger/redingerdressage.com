1cc63105941ace3761e11b7db5993f4359c55731
George Redinger <george@georgeredinger.com>
2012-07-10 10:50:02 -0700

    path typo

--- upload.sh
+++ upload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-scp rd.redinger.me.sql george@chicago.redinger.me:/home/george/workspace/redingerdressage.com/current/current
+scp rd.redinger.me.sql george@chicago.redinger.me:/home/george/workspace/redingerdressage.com/current/
 
