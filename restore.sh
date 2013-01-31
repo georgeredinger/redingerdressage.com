8e0cf82a3c4a9be8f457435d7d20ed7bb36ce093
George Redinger <george@georgeredinger.com>
2013-01-31 09:38:13 -0800

    dollar sign

--- restore.sh
+++ restore.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-$DB_PASSWORD=`grep DB_PASSWORD .htaccess | cut -d" " -f3`
+DB_PASSWORD=`grep DB_PASSWORD .htaccess | cut -d" " -f3`
 mysql -u redingerdressage -p$DB_PASSWORD redingerdressage < /home/george/workspace/redingerdressage.com/rd.redinger.me.sql
